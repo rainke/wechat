bc89b1f94f4f4e219a7e92b4676a850cdbe2c56a
unknown <>
2016-10-05 12:20:28 +0800

    access_token

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 var Koa = require('koa');
 var sha1 = require('sha1');
 var wechat = require('./wechat/g');
+var path = require('path');
+var util = require('./libs/util')
+
+var wechat_file = path.join(__dirname, './config/wechat.txt');
 var config = {
 	wechat:{
 		appId:'wx027405581ed8f1ee',
-		appScret: 'e1080f8f2494f830c7429a8b2d9c0998',
-		token: 'weixin'
+		appSecret: 'e1080f8f2494f830c7429a8b2d9c0998',
+		token: 'weixin',
+		getAccessToken: function() {
+			return util.readFileAsync(wechat_file);
+		},
+		saveAccessToken: function(data) {
+			data = JSON.stringify(data);
+			return util.writeFileAsync(wechat_file, data);
+		}
 	}
 }
 
