533a49ccebac1eaa9d06fcc07fa523254bed1078
unknown <>
2016-10-05 11:11:54 +0800

    fix

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var sha1 = require('sha1');
 var wechat = require('./wechat/g');
 var config = {
 	wechat:{
-		appId:'wx7aecd428bc14d399',
-		appScret: '6701c1e512c9840713d432f45dccdd7b',
+		appId:'wx027405581ed8f1ee',
+		appScret: 'e1080f8f2494f830c7429a8b2d9c0998',
 		token: 'weixin'
 	}
 }
@@ -12,7 +12,7 @@ var config = {
 var app = new Koa();
 
 app.use(wechat(config.wechat));
-app.listen('1234')
-// app.listen(80, '139.224.53.16');
+// app.listen('1234')
+app.listen(80, '139.224.53.16');
 
 console.log('start');
