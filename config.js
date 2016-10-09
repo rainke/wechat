37269b1ce98909b4c6d9d7a6eebc94d7262efef7
RAINKE <947798918@qq.com>
2016-10-09 23:18:28 +0800

    test account

--- config.js
+++ config.js
@@ -4,8 +4,10 @@ var util = require('./libs/util')
 var wechat_file = path.join(__dirname, './config/wechat.txt');
 var config = {
 	wechat:{
-		appID:'wx027405581ed8f1ee',
-		appSecret: 'e1080f8f2494f830c7429a8b2d9c0998',
+		// appID:'wx027405581ed8f1ee',
+		// appSecret: 'e1080f8f2494f830c7429a8b2d9c0998',
+		appID:'wx7aecd428bc14d399',
+		appSecret:'6701c1e512c9840713d432f45dccdd7b',
 		token: 'weixin',
 		getAccessToken: function() {
 			return util.readFileAsync(wechat_file);
