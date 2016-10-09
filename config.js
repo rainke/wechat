4ad6fa44e0b496692cc01c2f6ff578d4ea6668e0
RAINKE <947798918@qq.com>
2016-10-09 23:30:39 +0800

    ff

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ var config = {
 		// appSecret: 'e1080f8f2494f830c7429a8b2d9c0998',
 		appID:'wx7aecd428bc14d399',
 		appSecret:'6701c1e512c9840713d432f45dccdd7b',
-		token: 'weixin',
+		token: 'hepeng',
 		getAccessToken: function() {
 			return util.readFileAsync(wechat_file);
 		},
